bd8f3b79db8dddd4ec5590c117403e9d09342b76
fanmingfei <az8641683@163.com>
2017-08-01 17:38:46 +0800

    优化了模板功能

--- src/List.js
+++ src/List.js
@@ -4,18 +4,13 @@ export default class List extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            content: props.content,
-            status: props.status
+            content: props.item.content,
+            status: props.item.status
         }
-    }
-    complete() {
-        this.setState({
-            status: !this.state.status
-        })
     }
     render() {
         return Weakact.render`
-            <li onClick="this.complete" style="color: ${this.state.status ? 'red' : 'black'}">${this.state.content} </li>
+            <li onClick="${()=>this.props.changeStatus()}" style="color: ${this.state.status ? 'red' : 'black'}">${this.state.content} </li>
         `
     }
 }
--- src/index.js
+++ src/index.js
@@ -14,23 +14,28 @@ class TodoList extends Component {
             }]
         }
         setTimeout(()=> {
-            this.setState({
-                list: [{
-                    content: '实现一个简易版的 React 类似的东西 2333',
-                    status: true
-                },{
-                    content: '明天要去欢乐谷 饿死我了 3点了',
-                    status: false
-                },{
-                    content: '两秒后自动添加这一条',
-                    status: false
-                }]
+            this.addList({
+                content: '两秒后自动添加这一条',
+                status: false
             })
         },2000)
     }
 
+    addList(obj) {
+        this.setState({
+            list: [...this.state.list, ...[obj]]
+        })
+    }
+
     render () {
-        const list = this.state.list.map(x=>new List(x))
+        const list = this.state.list.map((x,i)=>new List({
+            changeStatus: ()=>{
+                const list = [...this.state.list]
+                list[i].status = !list[i].status
+                this.setState({list})
+            },
+            item: x
+        }))
         return Weakact.render`
             <div>${list}</div>
         `
